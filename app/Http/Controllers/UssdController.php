2d636e312bf1e1730bc01b74505f5edb3c729f70
William Andoh-Mensah <wandoh-mensah@itconsortiumgh.com>
2021-05-27 18:45:14 +0000

    fixed a bug

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -22,100 +22,58 @@ class UssdController extends Controller
 
     protected function menuStatus($text, $ussdStringExploded, $level)
     {
-        switch (isset($text)) {
+        switch ($text) {
                 case "":
-                    $this->welcomeMenu();
+                    $response = "CON Welcome to mAgric Farmer Onboarding".PHP_EOL;
+        $response .= "Are you a farmer ?".PHP_EOL;
+        $response .= "1. Yes".PHP_EOL;
+        $response .= "2. No".PHP_EOL;
                     break;
 
                 case "1":
-                    $this->farmerVerficationMenu();
+                    $response = "CON Name: Philip Appiah".PHP_EOL;
+                    $response .= "1. Confirm".PHP_EOL;
+                    $response .= "2. Cancel".PHP_EOL;
                     break;
 
                 case "1*1":
-                     $this->cropSelectionMenu();
+                    $response = "CON Select type of crop".PHP_EOL;
+                    $response .= "1. Cocoa".PHP_EOL;
+                    $response .= "2. Cashew".PHP_EOL;
+                    $response .= "3. Coffee".PHP_EOL;
+                    $response .= "4. Other".PHP_EOL;
                     break;
 
                 case ($ussdStringExploded[0] == 1 && $ussdStringExploded[1] == 1 && $level ==3):
-                      $this->quantitySelectionMenu();
+                    $response  = "CON Select quantity of last safe".PHP_EOL;
+                    $response .= "1. 0 - 54kg".PHP_EOL;
+                    $response .= "2. 55 - 124kg".PHP_EOL;
+                    $response .= "3. 125 - 254kg".PHP_EOL;
+                    $response .= "4. 255 - 500kg".PHP_EOL;
+                    $response .= "5. Greater than 500kg".PHP_EOL;
                     break;
 
                 case ($ussdStringExploded[0] == 1 && $ussdStringExploded[1] == 1 && $level ==4):
-                     $this->locationMenu();
+                    $response = "CON Enter the location of your farm".PHP_EOL;
                     break;
 
                 case ($ussdStringExploded[0] == 1 && $ussdStringExploded[1] == 1 && $level == 5):
-                     $this->LastMenu();
+                    $response = "END You have been" .PHP_EOL;
+                    $response .= "successfully registered " .PHP_EOL;
+                    $response .= "as a farmer" .PHP_EOL;
+                    $response .= "" .PHP_EOL;
+                    $response .= "Thank you" .PHP_EOL;
                     break;
 
 
                 case "1*2":
-                       $this->endMenu();
+                    $response = "END";
                         break;
                 default:
                       // response a user respond with input 2 from our first level
-                      $this->endMenu();
+                      $response = "END";
                     break;
             }
-        //echo $response;
-    }
-
-    private function welcomeMenu()
-    {
-        $response = "CON Welcome to mAgric Farmer Onboarding".PHP_EOL;
-        $response .= "Are you a farmer ?".PHP_EOL;
-        $response .= "1. Yes".PHP_EOL;
-        $response .= "2. No".PHP_EOL;
-        echo $response;
-    }
-
-    private function farmerVerficationMenu()
-    {
-        $response = "CON Name: Philip Appiah".PHP_EOL;
-        $response .= "1. Confirm".PHP_EOL;
-        $response .= "2. Cancel".PHP_EOL;
-        echo $response;
-    }
-
-    private function cropSelectionMenu()
-    {
-        $response = "CON Select type of crop".PHP_EOL;
-        $response .= "1. Cocoa".PHP_EOL;
-        $response .= "2. Cashew".PHP_EOL;
-        $response .= "3. Coffee".PHP_EOL;
-        $response .= "4. Other".PHP_EOL;
-        echo $response;
-    }
-
-    private function quantitySelectionMenu()
-    {
-        $response  = "CON Select quantity of last safe".PHP_EOL;
-        $response .= "1. 0 - 54kg".PHP_EOL;
-        $response .= "2. 55 - 124kg".PHP_EOL;
-        $response .= "3. 125 - 254kg".PHP_EOL;
-        $response .= "4. 255 - 500kg".PHP_EOL;
-        $response .= "5. Greater than 500kg".PHP_EOL;
-        echo $response;
-    }
-
-    private function LastMenu()
-    {
-        $response = "END You have been" .PHP_EOL;
-        $response .= "successfully registered " .PHP_EOL;
-        $response .= "as a farmer" .PHP_EOL;
-        $response .= "" .PHP_EOL;
-        $response .= "Thank you" .PHP_EOL;
-        echo $response;
-    }
-
-    private function locationMenu()
-    {
-        $response = "CON Enter the location of your farm".PHP_EOL;
-        echo $response;
-    }
-
-    private function endMenu()
-    {
-        $response = "END";
         echo $response;
     }
 }
